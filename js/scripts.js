1905dcc57354401dbe33c17b4350429d512c7fd9
Alex <shocer1991@gmail.com>
2020-03-30 11:13:12 +0300

    Added files

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function () { 
+    
+    // Делаем сортировку и пагинацию
+    $('#table_main').DataTable({
+        "lengthChange": false,
+        "pageLength": 3,
+        "searching": false
+    });
+
+    // Если авторизован под админом, то прячем окно авторизации и показываем кнопку чтобы разлогиниться
+    $.ajax({
+        url:'login/session_check',
+        method:"POST",
+        success:function(data){  
+            if (data == "Active") {
+                $("#autorization").hide();
+                $("#logoff").removeClass("d-none");
+                $(":checkbox").removeClass("d-none");
+            } 
+        }
+    });
+
+    $(".paginate_button").click(function(){
+        $(":checkbox").removeClass("d-none");
+    });
+
+    // Разлогиниваем по клику на кнопку
+    $("#logoff").click(function() {
+        $.ajax({
+           url:'login/logout'
+        });
+    });
+
+});
+
+
+
